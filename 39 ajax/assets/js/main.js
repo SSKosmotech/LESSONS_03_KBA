2a4e96243842e408d53e4fca3299260624fa83a9
Serhii Simonov <ss@kosmotech.com.ua>
2022-06-08 23:37:52 +0300

    Local storage cookie UP (39 AJAX)

--- 39 ajax/assets/js/main.js
+++ 39 ajax/assets/js/main.js
@@ -141,4 +141,85 @@ $(function(){
 
 
 
+//<<=======================Cookie==============================>>
 
+setCookie('my-js-cookie', 'some value here');
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+// const CART = [
+//     {
+//         "name": "Milk",
+//         "qty": 2,
+//         "price": 12.34,
+//         "isBuy": false
+//     },
+//     {
+//         "name": "Water",
+//         "qty": 3,
+//         "price": 10.34,
+//         "isBuy": false
+//     },
+//     {
+//         "name": "Bread",
+//         "qty": 1,
+//         "price": 17.34,
+//         "isBuy": false
+//     }
+// ]
+
+let CART = [];
+
+//Встановити значення у localStorage
+// localStorage.setItem('CART', JSON.stringify(CART))
+
+// (JSON.stringify(CART));  // щоб передати масив, переводимо його в строку шляхом JSON.stringify() - бо в localStorage можна вписувати лише строки
+// JSON.parse(JSON.stringify(CART)); //переводить строку в об'єкт
+
+// отримати дані з localStorage
+// const CART_FROM_STORAGE = localStorage.getItem('CART');
+// const CART_FROM_STORAGE = localStorage.getItem('CART2');
+
+// console.log('CART2:', CART_FROM_STORAGE); // CART2: null (не повинно дорівнювати null)
+
+
+const CART_FROM_STORAGE = JSON.parse(localStorage.getItem('CART'));
+if(CART_FROM_STORAGE !== null){
+    CART = CART_FROM_STORAGE;
+    // localStorage.removeItem('CART');
+}
+
+CART.push({
+    name: 'New prod',
+    qty: 123,
+    price: 12.32,
+    isBuy: false
+});
+
+localStorage.setItem('CART', JSON.stringify(CART));
+
+// console.log('CART', JSON.stringify(CART));
+// const CART_FROM_STORAGE = JSON.parse(localStorage.getItem('CART'));
+
+console.log('CART', CART);
